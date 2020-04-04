1b0aaf81ce5d5923fca60ba629ceb82792e10f5f
00400815@uca.edu.sv <16137238>
2020-04-03 21:21:07 -0600

    funct bisection update

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ void bisection(double a, double b, double TOL){
     int n = ceil(log2(( b - a) / TOL ) );
     for (int i = 0; i < n; ++i) {
         double p = ( b - a ) / 2.0;
-        cout<<i<<"\t"<<p<<endl;
+        cout<< setprecision(20)<<i+1<<"\t"<<p<<endl;
         if(f(a)*f(p) < 0)   b = p;
         else a = p;
 
