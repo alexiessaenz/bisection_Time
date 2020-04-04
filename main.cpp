60df1b3b70712ce1a27d0e1cfb9ebedd75ce8147
00400815@uca.edu.sv <16137238>
2020-04-03 20:11:59 -0600

    funct bisection

--- main.cpp
+++ main.cpp
@@ -4,6 +4,10 @@
 using namespace::std;
 double f(double x){     return sqrt(x);     }
 
+void bisection(double a, double b, double TOL){
+    int n = ceil(log2(( b - a) / TOL ) );
+}
+
 int main() {
     cout << "Hello, World!" << std::endl;
     return 0;
